802ea660435dcba1754e5c6dd2a0272fac0edb4a
kananthav <kevin.anantha@gmail.com>
2018-09-24 00:57:24 +0800

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'playing dota 2' }
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+    console.log(`todo that has ID: ${id} has been deleted!`);
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={ this.deleteTodo } />
+        <AddTodo addTodo={ this.addTodo } />
       </div>
     );
   }
---
+++ src/Todos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Todos = ({todos, deleteTodo}) => {
+
+  const TodoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span onClick={ () => { deleteTodo(todo.id) } }>{todo.content}</span>
+        </div>
+      )
+    })
+  ) : (
+    <p className="center">You have no todo's left, Yay!</p>
+  )
+  return (
+    <div className="todos collection">
+      {TodoList}
+    </div>
+  )
+}
+
+export default Todos;
